b11608a081751477671ee941b54db43adf2254d3
Mostafa kamal <110708254+mostafa188@users.noreply.github.com>
2022-09-16 17:19:45 +0600

    STL

---
+++ Bit Masking/Non Repeating Numbers/Vector.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> v) 
+    {
+        vector<int>n;
+        
+        for(int i=0;i<v.size();i++){
+            int p = count(v.begin(),v.end(),v[i]);
+            if(p == 1) n.push_back(v[i]);
+        }
+        sort(n.begin(),n.end());
+        
+        return n;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
