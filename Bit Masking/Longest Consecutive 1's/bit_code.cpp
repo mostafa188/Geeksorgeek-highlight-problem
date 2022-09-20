4e245092b92c8feb91ff09d8d72117bd82df5bb9
Mostafa kamal <110708254+mostafa188@users.noreply.github.com>
2022-09-21 02:20:53 +0600

    bit_code

---
+++ Bit Masking/Longest Consecutive 1's/bit_code.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int n)
+    {
+        int cnt =0;
+        
+        while(n!=0){
+            n = (n &(n<<1));
+            cnt++;
+        }
+        return cnt;
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
